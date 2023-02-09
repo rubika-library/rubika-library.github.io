7611b5c5b0d4a5b8f7fa19a08609fc4e9e9502e2
Amirreza <amirreza.firoz13@gmail.com>
2023-02-09 15:38:59 +0330

    fix menu bugs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 document.querySelector('header .button').addEventListener('click', e => {
     __toggleMenu();
+    document.querySelector('header .button').classList.toggle('button2');
 });
-if (isMobile()) {
+if (_isMobile_()) {
     alert(200);
     document.querySelector('nav ul').style.fontSize = 'small';
 }
