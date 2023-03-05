89ecdccc7ee6d3cba8d3dd27e31156712533af3d
Amirreza <amirreza.firoz13@gmail.com>
2023-03-05 14:55:09 +0330

    edit nav

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 const __toggleMenu = () => {
     let nav = document.getElementsByTagName('nav')[0];
-    nav.style.right = nav.style.right != '-65%' ? '-65%' : 'calc(-100% - 10px)';
+    nav.style.right = nav.style.right != '-65%' ? '-65%' : '0';
 };
 
 const _isMobile_ = () => {
